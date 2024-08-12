93adaa260740c036763fdbd2b8b3aafa0a876317
cristobalhiza <cristobalhiza@gmail.com>
2024-08-11 22:14:14 -0400

    agrega metodos restantes

--- src/dao/heroesManager.js
+++ src/dao/heroesManager.js
@@ -4,9 +4,49 @@ export class HeroesManager {
 
     static async getHeroes() {
         if (fs.existsSync(this.path)) {
-            return JSON.parse(await fs.readFileSync(this.path, {encoding:"utf-8"}))
-        }else{
+            return JSON.parse(await fs.readFileSync(this.path, { encoding: "utf-8" }))
+        } else {
             return []
         }
     }
+
+    static async addHeroe(heroe = {}) {
+        let heroes = await this.getHeroes()
+        let id = 1
+        if (heroes.length > 0) {
+            id = Math.max(...heroes.map(d => d.id)) + 1
+        }
+        let nuevoHeroe = {
+            id,
+            ...heroe //aquí los ... son el operador spread
+        }
+        heroes.push(nuevoHeroe)
+        await fs.promises.writeFile(this.path, JSON.stringify(heroes, null, 8))
+        return nuevoHeroe
+    }
+
+    static async updateHeroe(id, aModificar = {}) {
+        let heroes = await this.getHeroes()
+        let index = heroes.findIndex(d => d.id === id)
+        if (index === -1) {
+            throw new Error(`Error: no existe el id${id}`)
+        }
+        heroes[index] = { ...heroes[index], ...aModificar, id }
+        await fs.promises.writeFile(this.path, JSON.stringify(heroes, null, 8))
+        return heroes[index]
+    }
+
+    static async deleteHeroe(id) {
+        let heroes = await this.getHeroes()
+        let index = heroes.findIndex(d => d.id === id)
+        if (index === -1) {
+            throw new Error(`Error: no existe el id${id}`)
+        }
+        let cantidad0 = heroes.length
+        heroes = heroes.filter(h => h.id !== id)
+        let cantidad1 = heroes.length
+        await fs.promises.writeFile(this.path, JSON.stringify(heroes, null, 8))
+        return cantidad0 - cantidad1
+    }
 }
+
--- src/queryParams.js
+++ src/queryParams.js
@@ -1,45 +1,57 @@
-import express from "express"
+import express from "express";
 // import { heroes } from "./data/lotr.js"
-import { HeroesManager } from "./dao/heroesManager.js"
+import { HeroesManager } from "./dao/heroesManager.js";
 
-const PORT = 3000
+const PORT = 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //esta última línea permitirá que el servidor pueda interpretar mejor los datos complejos que viajen desde la url y mapearlos correctamente en el req.query
 
-HeroesManager.path = "./src/data/lotr.json"
+HeroesManager.path = "./src/data/lotr.json";
 
 app.get("/", (req, res) => {
-    res.setHeader('Content-Type', 'text/plain')
-    res.status(200).send('OK')
-})
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ error: "OK" });
+});
+//esta última se llama Manejador de ruta o Route handler
 
-app.get("/saludo", (req, res) => {
-    console.log(req.query)
+app.get("/api/saludo", (req, res) => {
+    console.log(req.query);
 
-    let { nombre, despedida } = req.query
+    let { nombre, despedida } = req.query;
     if (!nombre) {
-        return res.status(400).send("Complete nombre via query param.!")
+        return res.status(400).json({ error: "Complete nombre via query param.!" });
     }
 
-    let saludo = `${despedida ? "Chao" : "Hola"}, ${nombre}`
+    let saludo = `${despedida ? "Chao" : "Hola"}, ${nombre}`;
 
-    res.setHeader('Content-Type', 'text/plain')
-    res.status(200).send(saludo)
-})
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ saludo });
+});
 
-app.get("/heroes", async (req, res) => {
-    let heroes = await HeroesManager.getHeroes()
+app.get("/api/heroes", async (req, res) => {
+    let heroes;
+    try {
+        heroes = await HeroesManager.getHeroes();
+    } catch (error) {
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({
+            error: "Error inesperado en el servidor - Intente más tarde",
+            detallle: `${error.message}`,
+        });
+    }
 
     let { limit, skip } = req.query;
 
     if (limit) {
         limit = Number(limit);
         if (isNaN(limit) || limit < 0) {
-            return res.status(400).send("Limit debe ser un número entero mayor o igual a 0");
+            return res
+                .status(400)
+                .json({ error: "Limit debe ser un número entero mayor o igual a 0" });
         }
     } else {
         limit = heroes.length;
@@ -48,30 +60,159 @@ app.get("/heroes", async (req, res) => {
     if (skip) {
         skip = Number(skip);
         if (isNaN(skip) || skip < 0) {
-            return res.status(400).send("Skip debe ser un número entero mayor o igual a 0");
+            return res
+                .status(400)
+                .json({ error: "Skip debe ser un número entero mayor o igual a 0" });
         }
     } else {
         skip = 0;
     }
 
     let resultado = heroes.slice(skip, skip + limit);
-
-    res.status(200).send(resultado);
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ resultado });
 });
 
-app.get("/heroes/:id", async (req, res) => {
-    let { id } = req.params
-    id = Number(id)
+app.get("/api/heroes/:id", async (req, res) => {
+    let { id } = req.params;
+    id = Number(id);
     if (isNaN(id)) {
-        return res.status(400).send("id debe ser un número")
+        return res.status(400).json({ error: "id debe ser un número" });
+    }
+    // let heroes = await HeroesManager.getHeroes()
+    let heroes;
+    try {
+        heroes = await HeroesManager.getHeroes();
+    } catch (error) {
+        console.log(error);
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({
+            error: "Error inesperado en el servidor - Intente más tarde",
+            detallle: `${error.message}`,
+        });
+    }
+
+    let heroe = heroes.find((h) => h.id === id);
+    if (!heroe) {
+        return res.status(404).json({ error: `Heroe con id ${id} no encontrado` });
+    }
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ heroe });
+});
+
+app.post("/api/heroes/", async (req, res) => {
+    let { name, ...otros } = req.body //...es aquí el operador rest
+    if (!name) {
+        res.setHeader("Content-Type", "application/json");
+        return res.status(400).json({ error: "El nombre del héroe es requerido" })
     }
     let heroes = await HeroesManager.getHeroes()
-    let heroe = heroes.find(h => h.id === id)
-    if (!heroe) {
-        return res.status(404).send(`Heroe con id ${id} no encontrado`);
+    let existe = heroes.find(h => h.name.toLowerCase() === name.toLowerCase())
+    if (existe) {
+        res.setHeader("Content-Type", "application/json");
+        return res.status(400).json({ error: `Ya existe un personaje llamado ${name}` });
     }
-    res.status(200).send(heroe)
+    //Validar lo que se necesite
+    try {
+        let heroeNuevo = await HeroesManager.addHeroe({ name, ...otros })
+        res.setHeader("Content-Type", "application/json");
+        return res.status(201).json({ heroeNuevo });
+    } catch (error) {
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({ error: "Error inesperado en el servidor - Intente nuevamente" })
+    }
 })
 
+app.put("/api/heroes/:id", async (req, res) => {
+    let { id } = req.params;
+    id = Number(id);
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "id debe ser un número" });
+    }
+    // let heroes = await HeroesManager.getHeroes()
+    let heroes;
+    try {
+        heroes = await HeroesManager.getHeroes();
+    } catch (error) {
+        console.log(error);
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({
+            error: "Error inesperado en el servidor - Intente más tarde",
+            detallle: `${error.message}`,
+        });
+    }
+
+    let heroe = heroes.find((h) => h.id === id);
+    if (!heroe) {
+        return res.status(404).json({ error: `Heroe con id ${id} no encontrado` });
+    }
+    let { ...aModificar } = req.body;
+
+    delete aModificar.id;
+    //validaciones 
+    if (aModificar.name) {
+        let existe = heroes.find(h => h.name.toLowerCase() === aModificar.name.toLowerCase())
+        if (existe) {
+            res.setHeader("Content-Type", "application/json");
+            return res.status(400).json({ error: `Ya existe un personaje llamado ${aModificar.name}` })
+        }
+    }
+    try {
+        let heroeModificado = await HeroesManager.updateHeroe(id, aModificar)
+        res.setHeader("Content-Type", "application/json");
+        return res.status(200).json({ heroeModificado })
+    } catch (error) {
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({ error: "Error inesperado en el servidor - Intente nuevamente" })
+    }
+});
+
+app.delete("/api/heroes/:id", async (req, res) => {
+    let { id } = req.params;
+    id = Number(id);
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "id debe ser un número" });
+    }
+    try {
+        let resultado = await HeroesManager.deleteHeroe(id)
+        if(resultado>0){
+            res.setHeader("Content-Type", "application/json");
+            return res.status(200).json({payload:"Personaje Elimimnado!"})
+        } else {
+            res.setHeader("Content-Type", "application/json");
+            return res.status(404).json({ error: `Heroe con id ${id} no eliminado :(`})
+        }
+    } catch (error) {
+        console.log(error);
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({
+            error: "Error inesperado en el servidor - Intente Nuevamente",
+            detallle: `${error.message}`
+        }
+        )
+    }
+
+});
+
 const server = app.listen(PORT, () =>
-    console.log(`Server online en puerto ${PORT}`))
+    console.log(`Server online en puerto ${PORT}`)
+);
+
+//Metodos (usar Postman)
+let nombre = "Cristobal";
+
+app.get("/api/nombre", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    return res.status(200).json({ nombre });
+});
+
+app.post("/api/nombre/", (req, res) => {
+    let { nombre: nombreBody } = req.body;
+    if (!nombreBody) {
+        res.setHeader("Content-Type", "application/json");
+        return res.status(400).json({ error: "Complete nombre" });
+    }
+    nombre = `${nombre} ${nombreBody}`.trim();
+    res.setHeader("Content-Type", "application/json");
+    return res.status(200).json({ nombre });
+});
